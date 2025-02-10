8509c3704d9e94939af5a543c28bdb32b5747049
Zekepk45 <zekepk456@gmail.com>
2025-02-10 10:21:24 -0700

    full adder VHDL

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -48,6 +48,12 @@ begin
        -- Test all ones input
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"E" and w_Cout = '1') report "bad with ones" severity failure;
+       w_addends <= x"00"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"1" and w_Cout = '0') report "bad with ones" severity failure;
+       w_addends <= x"29"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"B" and w_Cout = '0') report "bad with ones" severity failure;
+       w_addends <= x"34"; w_Cin <= '1'; wait for 10 ns;
+	       assert (w_sum = x"8" and w_Cout = '0') report "bad with ones" severity failure;
        -- TODO, a few other test cases
 	
 		wait; -- wait forever
